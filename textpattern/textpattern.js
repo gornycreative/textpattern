c73b830a19795a698fc8a9abc2b984d1f6de6ba8
Phil Wareham <phil@designhive.com>
2014-12-03 09:39:32 +0000

    Date picker isn't going to work well for us - removing
    Will add back in as a experimental branch sometime.

--- textpattern/textpattern.js
+++ textpattern/textpattern.js
@@ -1691,36 +1691,6 @@ $(document).ready(function ()
 
     // TODO: integrate jQuery UI stuff properly --------------------------------
 
-    // Date picker.
-    // TODO support from RTL languages
-    $('.input-day').datepicker({
-        showOn: 'button',
-        beforeShow: function (dateText, picker)
-        {
-            var me = $(this);
-            var year = me.siblings('.input-year').val();
-            var month = me.siblings('.input-month').val();
-            var day = me.val();
-            var showDate = year + '/' + month + '/' + day;
-
-            return {defaultDate: showDate};
-        },
-        buttonImageOnly: false,
-        dateFormat: 'yy/mm/dd',
-        onClose: function (dateText, picker)
-        {
-            var me = $(this);
-            var pickerDate = me.datepicker('getDate');
-            var year = pickerDate.getFullYear();
-            var month = pickerDate.getMonth() + 1;
-            var day = pickerDate.getDate();
-
-            me.siblings('.input-year').val(year);
-            me.siblings('.input-month').val(String('00' + month).slice(-2));
-            me.val(String('00' + day).slice(-2));
-        }
-    });
-
     // Selectmenu
     $('.jquery-ui-selectmenu').selectmenu();
 
